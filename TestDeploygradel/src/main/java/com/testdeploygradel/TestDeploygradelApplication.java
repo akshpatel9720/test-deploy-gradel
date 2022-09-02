4edb2461274f3ec347941bb51fd8d95309a98f75
akshpatel9720 <akshpatel9720@gmail.com>
2022-09-02 11:34:12 +0530

    first commit

---
+++ TestDeploygradel/src/main/java/com/testdeploygradel/TestDeploygradelApplication.java
@@ -0,0 +1,13 @@
+package com.testdeploygradel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDeploygradelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDeploygradelApplication.class, args);
+	}
+
+}
---
+++ TestDeploygradel/src/test/java/com/testdeploygradel/TestDeploygradelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testdeploygradel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDeploygradelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
